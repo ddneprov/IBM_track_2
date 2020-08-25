cc642ecef4dd5d09cf0172f99a462fd243594127
AdilBikeev <bikeev1999@mail.ru>
2020-08-26 00:56:59 +0300

    WEB-1 Поправил отображение Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { makeStyles, createStyles } from "@material-ui/core/styles"
-import { ButtonBase, Theme, Toolbar } from "@material-ui/core";
+import { ButtonBase, Theme } from "@material-ui/core";
 import { IBM_Default_Color } from "../base/types/ColorBase";
 
 const ibm_logo = require("../assets/ibm_logo.svg")
@@ -23,11 +23,11 @@ export const Header = () => {
   const classes = useStyles()
 
   return (
-    <Toolbar className={classes.header}>
+    <div className={classes.header}>
       <ButtonBase
         className={classes.header__logo}>
         <img src={ibm_logo} alt="logo" />
       </ButtonBase>
-    </Toolbar>
+    </div>
   )
 }
