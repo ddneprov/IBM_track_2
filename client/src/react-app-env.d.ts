4d750367984cbcf7cf9b9b4dafbba52187a5cb43
AdilBikeev <bikeev1999@mail.ru>
2020-09-04 17:07:18 +0300

    WEB-3 Добавил глобальный объект для регулирвоания настроек приложения

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+/**
+ * Создает объект с конфигурациями приложения.
+ */
+class Config {
+    private DebugEnable: false
+
+    public Config(DebugEnable) {
+        this.DebugEnable = DebugEnable
+    }
+
+    /**
+     * true, елси использовать настройки для отладки
+     */
+    public getDebugEnable = () => DebugEnable
+}
+
+export const config = new Config(true)
