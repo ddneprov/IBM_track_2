850166a6f09b25e2d770e0f5e359a07146338ecc
Dneprov <44896272+ddneprov@users.noreply.github.com>
2020-09-09 23:24:22 +0300

    getAllPilots controller

--- server/src/main/java/com/IBMiX2/server/controllers/ManagerRestControllerV1.java
+++ server/src/main/java/com/IBMiX2/server/controllers/ManagerRestControllerV1.java
@@ -1,8 +1,11 @@
 package com.IBMiX2.server.controllers;
 
 import com.IBMiX2.server.domain.User;
+import com.IBMiX2.server.dto.UserDto;
 import com.IBMiX2.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,4 +26,22 @@ public class ManagerRestControllerV1 {
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
+
+    @RequestMapping(value = "getAllPilots", method = RequestMethod.GET)
+    public List<User> getAllPilot(){
+        return userService.getAllPilots();
+    }
+
+    @GetMapping(value = "getUser/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Integer id){
+        User user = userService.findUserById(id);
+
+        if(user == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        UserDto result = UserDto.fromUser(user);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
 }
