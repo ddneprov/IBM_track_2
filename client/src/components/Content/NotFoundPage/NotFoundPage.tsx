95d4eacb67f983cf61732e895b7c6b67bde37b1a
AdilBikeev <bikeev1999@mail.ru>
2020-09-14 18:39:01 +0300

    WEB-7 Code review

--- client/src/components/Content/NotFoundPage/NotFoundPage.tsx
+++ client/src/components/Content/NotFoundPage/NotFoundPage.tsx
@@ -1,20 +1,22 @@
 import React from "react"
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles"
+import { IBM_Default_Color } from "../../../base/types/ColorBase";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-      error404Wrapper: {
+      error__container: {
         textAlign: "center",
         padding: "20px 30px",
-        background: "#ffffff",
+        background: IBM_Default_Color.white,
+        borderRadius: '20px',
         marginTop: "10px"
       },
-      statusHeader: {
+      error__container_header: {
         fontSize: "35px",
         margin: "0",
         marginBottom: "15px"
       },
-      statusText: {
+      error__container_message: {
         fontSize: "25px",
         margin: "0",
       }
@@ -25,9 +27,9 @@ export const NotFoundPage: React.FC = () => {
     const classes = useStyles()
     
     return (
-      <div className={classes.error404Wrapper}>
-        <p className={classes.statusHeader}>404</p>
-        <p className={classes.statusText}>PAGE NOT FOUND</p>
+      <div className={classes.error__container}>
+        <p className={classes.error__container_header}>404</p>
+        <p className={classes.error__container_message}>PAGE NOT FOUND</p>
       </div>
     )
 }
