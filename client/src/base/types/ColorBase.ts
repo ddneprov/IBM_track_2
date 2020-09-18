e0f4a495fdb5f671a88c21d1189bfcd1b37459c8
AdilBikeev <bikeev1999@mail.ru>
2020-09-18 15:54:10 +0300

    WEB-Design Обновление палитры цветов

--- client/src/base/types/ColorBase.ts
+++ client/src/base/types/ColorBase.ts
@@ -2,7 +2,7 @@
  * Цвета для IBM по умолчанию
  */
 export enum IBM_Default_Color {
-    black = '#000',
+    black = 'hsl(0%, 0%, 2%)',
     white = '#F5F5F5',
     gray = '#eee',
     blue = '#0062ff',
@@ -10,9 +10,9 @@ export enum IBM_Default_Color {
 }
 
 export enum Seniority_Color {
-    black = '#000',
+    black = 'hsl(0%, 0%, 2%)',
     green = '#85E7B1',
     yellow = '#FBE980',
-    orange = '#FF7F0E',
+    orange = 'hsl(0, 0%, 64%)',
     red = '#F00',
 }
