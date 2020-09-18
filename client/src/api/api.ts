52398d93e19bdc7741ade0ca31417a8ee46b1f8f
AdilBikeev <bikeev1999@mail.ru>
2020-09-18 14:44:51 +0300

    hot-fix Add headers for CORS

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -4,5 +4,11 @@
  * Баззовое представление объекта для совершения запросов.
  */
 export const instance = axios.create({
-    baseURL: 'http://130.193.38.154:1337/api/v1'
+    baseURL: 'http://130.193.38.154:1337/api/v1',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+        'Access-Control-Allow': '*',
+        'Access-Control-Max-Age': '3600',
+    }
 })
